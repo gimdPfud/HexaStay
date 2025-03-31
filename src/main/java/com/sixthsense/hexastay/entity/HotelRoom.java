bf92d6eb238b7228f565aea02e5bfb08c3cfb43a
예령 <gimdPfud@gmail.com>
2025-03-31 15:53:25 +0900

    엔티티 파일 만들기만 함 (컬럼 추가 필요)

--- src/main/java/com/sixthsense/hexastay/entity/HotelRoom.java
+++ src/main/java/com/sixthsense/hexastay/entity/HotelRoom.java
@@ -1,13 +1,12 @@
 /***********************************************
- * 클래스명 : HotelRoomDTO
- * 기능 : HotelRoomDTO 엔티티
- * 작성자 : 김예령
+ * 클래스명 : HotelRoom
+ * 기능 : HotelRoom 엔티티
+ * 작성자 :
  * 작성일 : 2025-03-31
- * 수정 : 2025-03-31 BaseEntity 추가, 기존 날짜 필드 삭제, 상태 체크인 체크아웃 필드 추가 : 김예령
+ * 수정 : 2025-03-31
  * ***********************************************/
 package com.sixthsense.hexastay.entity;
 
-import com.sixthsense.hexastay.entity.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,48 +17,9 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "hotelRoom")
-public class HotelRoom extends BaseEntity {
+public class HotelRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "hotelRoomNum")
-    private Long hotelRoomNum;                  //번호
-
-    @Column(name = "hotelRoomName")
-    private String hotelRoomName;               //방 이름
-
-    @Column(name = "hotelRoomPhone")
-    private String hotelRoomPhone;              //방 전화번호
-
-    @Column(name = "hotelRoomType")
-    private String hotelRoomType;               //종류 (싱글 더블 스위트 ..)
-
-    @Column(name = "hotelRoomQr")
-    private String hotelRoomQr;                 //Qr명
-
-    @Column(name = "hotelRoomContent")
-    private String hotelRoomContent;            //방 상세설명
-
-    @Column(name = "hotelRoomPrice")
-    private Integer hotelRoomPrice;              //가격
-
-    @Column(name = "hotelRoomPassword")
-    private String hotelRoomPassword;           //비밀번호
-
-    @Column(name = "hotelRoomStatus")
-    private boolean hotelRoomStatus;            //활성화 상태
-
-    @Column(name = "hotelRoomCheckIn")
-    private String hotelRoomCheckIn;            //체크인
-
-    @Column(name = "hotelRoomCheckOut")
-    private String hotelRoomCheckOut;           //체크아웃
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "branchNum")
-    private Branch branch;             //지사 참조
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "facilityNum")
-    private Facility facility;         //시설 참조
+    private Long hotelRoomNum;
 }
--- src/main/java/com/sixthsense/hexastay/entity/Image.java
+++ src/main/java/com/sixthsense/hexastay/entity/Image.java
@@ -1,9 +1,9 @@
 /***********************************************
- * 클래스명 : ImageDTO
- * 기능 : ImageDTO 엔티티 (todo 추후 수정)
- * 작성자 : 김예령
+ * 클래스명 : Image
+ * 기능 : Image 엔티티
+ * 작성자 :
  * 작성일 : 2025-03-31
- * 수정 : 2025-03-31 카테고리 컬럼 두 개 추가 : 김예령
+ * 수정 : 2025-03-31
  * ***********************************************/
 package com.sixthsense.hexastay.entity;
 
@@ -22,15 +22,4 @@ public class Image {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "imageNum")
     private Long imageNum;
-
-    @Column(name = "imageName")
-    private String imageName;       //이미지 이름
-
-    @Column(name = "imageUrl")
-    private String imageUrl;        //이미지 경로
-
-    @Column(name = "imageSuperEntity")
-    private String imageSuperEntity;//카테고리 1번 : 어디 테이블?
-    @Column(name = "imageSuperNum")
-    private String imageSuperNum;   //카테고리 2번 : 그 중 어느 row?
 }
