80175bfffb0e38ceb465f8369d459cdd09e4caac
예령 <gimdPfud@gmail.com>
2025-03-31 17:34:57 +0900

    엔티티 DTO Repository 생성

--- src/main/java/com/sixthsense/hexastay/entity/Board.java
+++ src/main/java/com/sixthsense/hexastay/entity/Board.java
@@ -1,7 +1,7 @@
 /***********************************************
- * 클래스명 : Board
- * 기능 : Board 엔티티
- * 작성자 :
+ * 클래스명 : BoardDTO
+ * 기능 : BoardDTO 엔티티
+ * 작성자 : 김예령
  * 작성일 : 2025-03-31
  * 수정 : 2025-03-31
  * ***********************************************/
@@ -9,6 +9,10 @@ package com.sixthsense.hexastay.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -21,5 +25,26 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "boardNum")
-    private Long boardNum;
+    private Long boardNum;                  //번호
+
+    @Column(name = "boardTitle")
+    private String boardTitle;              //제목
+
+    @Column(name = "boardContent")
+    private String boardContent;            //내용
+
+    @Column(name = "boardWriter")
+    private String boardWriter;             //작성자
+
+    @CreatedDate
+    @Column(name = "boardCreateDate")
+    private LocalDateTime boardCreateDate;  //등록일자
+
+    @LastModifiedDate
+    @Column(name = "boardModifyDate")
+    private LocalDateTime boardModifyDate;  //수정일자
+
+    @Column(name = "boardView")
+    private Integer boardView;              //조회수
+
 }
--- src/main/java/com/sixthsense/hexastay/entity/Branch.java
+++ src/main/java/com/sixthsense/hexastay/entity/Branch.java
@@ -1,7 +1,7 @@
 /***********************************************
- * 클래스명 : Branch
- * 기능 : Branch 엔티티
- * 작성자 :
+ * 클래스명 : BranchDTO
+ * 기능 : BranchDTO 엔티티
+ * 작성자 : 김예령
  * 작성일 : 2025-03-31
  * 수정 : 2025-03-31
  * ***********************************************/
@@ -9,6 +9,9 @@ package com.sixthsense.hexastay.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -21,5 +24,28 @@ public class Branch {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "branchNum")
-    private Long branchNum;
+    private Long branchNum;                 //번호
+
+    @Column(name = "branchName")
+    private String branchName;              //이름
+
+    @Column(name = "branchPhone")
+    private String branchPhone;             //전화번호
+
+    @Column(name = "branchEmail")
+    private String branchEmail;             //이메일
+
+    @Column(name = "branchAddress")
+    private String branchAddress;           //주소
+
+    @Column(name = "branchCeoName")
+    private String branchCeoName;           //대표명
+
+    @Column(name = "branchCreateDate")
+    @CreatedDate
+    private LocalDateTime branchCreateDate; //등록일자
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "centerNum")
+    private Center branchCenter;            //센터 참조
 }
