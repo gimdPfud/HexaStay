e787d1f8b05a180a784feee350bf843c36019cd6
예령 <gimdPfud@gmail.com>
2025-04-01 15:36:13 +0900

    StoreService -> StoreMenu 이름 변경!!

--- src/main/java/com/sixthsense/hexastay/entity/StoreMenu.java
+++ src/main/java/com/sixthsense/hexastay/entity/StoreMenu.java
@@ -3,7 +3,7 @@
  * 기능 : StoreMenu 엔티티
  * 작성자 : 김부환
  * 작성일 : 2025-03-31
- * 수정 : 2025-03-31 BaseEntity 추가, 기존 날짜 필드 삭제 : 김예령
+ * 수정 : 2025-04-01 이름 변경 : 김예령
  * ***********************************************/
 package com.sixthsense.hexastay.entity;
 
@@ -21,23 +21,23 @@ import lombok.*;
 public class StoreMenu extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "storeServiceNum")
-    private Long storeServiceNum;
+    @Column(name = "storeMenuNum")
+    private Long storeMenuNum;
 
     //룸서비스외부 업체 상품이름
-    private String storeServicesName;
+    private String storeMenuName;
 
     //룸서비스외부 업체상품 가격
-    private Long storeServicePrice;
+    private Long storeMenuPrice;
 
     //룸서비스외부 업체상품 설명
-    private String storeServiceContent;
+    private String storeMenuContent;
 
     //룸서비스외부 업체상품 카테고리
-    private String storeServiceCategory;
+    private String storeMenuCategory;
 
     //룸서비스외부 업체상품 서비스 활성화 여부
-    private String storeServiceStatus;
+    private String storeMenuStatus;
 
     //참조 테이블 - Store 테이블 Pk 참조
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/sixthsense/hexastay/repository/StoreMenuRepository.java
+++ src/main/java/com/sixthsense/hexastay/repository/StoreMenuRepository.java
@@ -1,19 +1,19 @@
 /***********************************************
- * 인터페이스명 : StoreServiceRepository
+ * 인터페이스명 : StoreMenuRepository
  * 기능 :
- * 작성자 :
+ * 작성자 : 김예령
  * 작성일 : 2025-03-31
- * 수정 : 2025-03-31
+ * 수정 : 2025-04-01 이름 변경 : 김예령
  * ***********************************************/
 package com.sixthsense.hexastay.repository;
 
-import com.sixthsense.hexastay.entity.StoreService;
+import com.sixthsense.hexastay.entity.StoreMenu;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface StoreServiceRepository extends JpaRepository<StoreService, Long> {
-    @Query("select a from StoreService a")
-    public Page<StoreService> findAll(Pageable pageable);
+public interface StoreMenuRepository extends JpaRepository<StoreMenu, Long> {
+    @Query("select a from StoreMenu a")
+    public Page<StoreMenu> findAll(Pageable pageable);
 }
