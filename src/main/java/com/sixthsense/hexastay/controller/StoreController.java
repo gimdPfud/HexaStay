020e8c5af1a92dab80d51d579d103101b6173821
예령 <gimdPfud@gmail.com>
2025-04-04 13:17:18 +0900

    레이아웃 적용중...

--- src/main/java/com/sixthsense/hexastay/controller/StoreController.java
+++ src/main/java/com/sixthsense/hexastay/controller/StoreController.java
@@ -42,7 +42,7 @@ public class StoreController {
     public String insert(StoreDTO storeDTO){
         log.info("등록post : "+storeDTO);
         storeService.insert(storeDTO);
-        return "store/list";
+        return "redirect:/store/list";
     }
 
 
