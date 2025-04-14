9c38a7e0d5411f3eaa79c9f47b94523b89df593d
sihu <si@hu.so>
2025-04-14 09:59:19 +0900

    레이아웃수정신나갈거지같아-faq 및 리뷰 제거

--- src/main/java/com/sixthsense/hexastay/service/ReviewLikeService.java
+++
@@ -1,7 +0,0 @@
-package com.sixthsense.hexastay.service;
-
-import com.sixthsense.hexastay.dto.ReviewDTO;
-
-public interface ReviewLikeService {
-    public Integer toggleReviewLike(Long reviewNum, String memberEmail);
-}
