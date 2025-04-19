38488e61efb5650b7314b79d95ed87ca98038b03
pik <pik0329@naver.com>
2025-04-19 23:49:55 +0900

    aa

--- src/main/java/com/sixthsense/hexastay/controller/SettleController.java
+++ src/main/java/com/sixthsense/hexastay/controller/SettleController.java
@@ -1,12 +1,13 @@
 package com.sixthsense.hexastay.controller;
 
 import com.sixthsense.hexastay.dto.CompanyDTO;
+import com.sixthsense.hexastay.dto.HotelRoomDTO;
+import com.sixthsense.hexastay.dto.RoomDTO;
 import com.sixthsense.hexastay.dto.StoreDTO;
 import com.sixthsense.hexastay.entity.Company;
-import com.sixthsense.hexastay.service.AdminService;
-import com.sixthsense.hexastay.service.CompanyService;
-import com.sixthsense.hexastay.service.SettleService;
-import com.sixthsense.hexastay.service.StoreService;
+import com.sixthsense.hexastay.repository.AdminRepository;
+import com.sixthsense.hexastay.repository.HotelRoomRepository;
+import com.sixthsense.hexastay.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -28,11 +29,20 @@ public class SettleController {
     private final AdminService adminService;
     private final SettleService settleService;
     private final StoreService storeService;
+    private final AdminRepository adminRepository;
+    private final HotelRoomRepository hotelRoomRepository;
+    private final HotelRoomService hotelRoomService;
 
     @GetMapping("/chart")
-    public String chart(Principal principal, Model model)
+    public String chart(Principal principal, Model model, Pageable pageable)
     {
 
+        Long companyNum = adminRepository.findByAdminEmail(principal.getName()).getCompany().getCompanyNum();
+        log.info("1차확인" + companyNum);
+        Page<RoomDTO> roomDTOList = settleService.getSettleList(companyNum, pageable);
+        log.info("2차확인" + roomDTOList.getTotalElements());
+        model.addAttribute("roomDTOList", roomDTOList);
+        log.info("roomDTOList: {}", roomDTOList.getContent().get(0).getHotelRoomDTO());
         return "/settle/chart";
     }
 
