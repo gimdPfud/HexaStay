c34ec9d353be46f761afc65afa4ebb7d9e9eabae
yungyeom <kyg6389@naver.com>
2025-04-24 10:48:40 +0900

    호텔 룸캐어 서비스 기능등록

---
+++ src/main/java/com/sixthsense/hexastay/entity/RoomCare.java
@@ -0,0 +1,32 @@
+package com.sixthsense.hexastay.entity;
+import com.sixthsense.hexastay.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class RoomCare extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Long roomCareNum; // 룸메뉴 케어 넘버 pk
+
+    @ManyToOne
+    private Member member; // 회원일 경우만
+
+    @ManyToOne(optional = false)
+    private HotelRoom hotelRoom; // 항상 필요
+
+    private String roomMenuRequestMessage; // 요구사항
+
+    @Column(nullable = false)
+    private Boolean isGuest; // 비회원 여부
+
+}
---
+++ src/main/java/com/sixthsense/hexastay/service/RoomCareService.java
@@ -0,0 +1,9 @@
+package com.sixthsense.hexastay.service;
+
+import com.sixthsense.hexastay.dto.RoomCareDTO;
+
+public interface RoomCareService {
+
+    public void RoomCareInsert(RoomCareDTO roomCareDTO);
+
+}
