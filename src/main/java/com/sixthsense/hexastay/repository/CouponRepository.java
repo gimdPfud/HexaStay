e02c2ed82a13f610e81397e5ace91bb2f3136920
yungyeom <kyg6389@naver.com>
2025-04-25 10:59:05 +0900

    쿠폰 등록 중간편(50%완료)

--- src/main/java/com/sixthsense/hexastay/repository/CouponRepository.java
+++ src/main/java/com/sixthsense/hexastay/repository/CouponRepository.java
@@ -2,6 +2,8 @@ package com.sixthsense.hexastay.repository;
 
 import com.sixthsense.hexastay.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -22,6 +24,12 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     // 이미 쿠폰 발급 여부 확인
     boolean existsByMember_MemberEmailAndType(String memberEmail, String type);
+
+    // 유효한 쿠폰 목록만 가져오자.
+    @Query("SELECT c FROM Coupon c WHERE c.member.memberEmail = :email " +
+            "AND (c.used = false OR (c.repeatCouponCount IS NOT NULL AND c.repeatCouponCount > 0)) " +
+            "AND c.expirationDate >= CURRENT_DATE")
+    List<Coupon> findUsableCouponsByMemberEmail(@Param("email") String email);
 
 
 }
