406bfb4d843eba6108f5451f305a8eeb09f9c72a
예령 <gimdPfud@gmail.com>
2025-05-01 16:58:43 +0900

    25 05 01 옵션 개수 수정 2

--- src/main/java/com/sixthsense/hexastay/service/impl/StoremenuServiceImpl.java
+++ src/main/java/com/sixthsense/hexastay/service/impl/StoremenuServiceImpl.java
@@ -156,6 +156,7 @@ public class StoremenuServiceImpl implements StoremenuService {
                                 StoremenuOptionDTO optionDTO = new StoremenuOptionDTO(option);
                                 return optionDTO;
                             })
+                            .filter(optionDTO->optionDTO.getStoremenuOptionStatus().equals("alive"))
                             .toList());
             return menuDTO;
         }).toList();
@@ -173,6 +174,7 @@ public class StoremenuServiceImpl implements StoremenuService {
                                 StoremenuOptionDTO optionDTO = new StoremenuOptionDTO(option);
                                 return optionDTO;
                             })
+                            .filter(optionDTO->optionDTO.getStoremenuOptionStatus().equals("alive"))
                             .toList());
             return menuDTO;
         }).toList();
@@ -190,6 +192,7 @@ public class StoremenuServiceImpl implements StoremenuService {
                                 StoremenuOptionDTO optionDTO = new StoremenuOptionDTO(option);
                                 return optionDTO;
                             })
+                            .filter(optionDTO->optionDTO.getStoremenuOptionStatus().equals("alive"))
                             .toList());
             return menuDTO;
         }).toList();
