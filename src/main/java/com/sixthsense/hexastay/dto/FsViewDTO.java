4ad52f90d0a9f12f1cd4628f325bfdd07b2f82d7
예령 <gimdPfud@gmail.com>
2025-05-01 16:51:45 +0900

    25 05 01 옵션 개수 수정

--- src/main/java/com/sixthsense/hexastay/dto/FsViewDTO.java
+++ src/main/java/com/sixthsense/hexastay/dto/FsViewDTO.java
@@ -21,11 +21,24 @@ public class FsViewDTO {
     private Long facilNum;
     private String facilName;
     private String facilAddress;
+    private String facilLatitude;             //좌표1 위도?
+    private String facilLongitude;            //좌표2 경도?
+    private String facilWtmX;                 //좌표3 자세한 x좌표
+    private String facilWtmY;                 //좌표4 자세한 y좌표
+    private String facilPictureMeta;
     private List<FsssViewDTO> fssList = new ArrayList<>();
 
     public FsViewDTO(CompanyDTO cdto) {
         this.facilNum = cdto.getCompanyNum();
         this.facilName = cdto.getCompanyName();
         this.facilAddress = cdto.getCompanyAddress();
+        this.facilLatitude = cdto.getCompanyLatitude();             //좌표1 위도?
+        this.facilLongitude = cdto.getCompanyLongitude();           //좌표2 경도?
+        this.facilWtmX = cdto.getCompanyWtmX();                     //좌표3 자세한 x좌표
+        this.facilWtmY = cdto.getCompanyWtmY();                     //좌표4 자세한 y좌표
+        this.facilPictureMeta = cdto.getCompanyPictureMeta();
+    }
+    public void addFssList(FacilitiesDTO dto){
+        fssList.add(new FsssViewDTO(dto));
     }
 }
