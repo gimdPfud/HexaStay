1937746b0b5aeabc0c146d7b3cab803a7bd3cdea
예령 <gimdPfud@gmail.com>
2025-05-01 12:55:39 +0900

    25 05 01 컨트롤러 zz서비스 수정

--- src/main/java/com/sixthsense/hexastay/service/ZzService.java
+++ src/main/java/com/sixthsense/hexastay/service/ZzService.java
@@ -16,6 +16,7 @@ import com.sixthsense.hexastay.repository.MemberRepository;
 import com.sixthsense.hexastay.repository.RoomRepository;
 import com.sixthsense.hexastay.service.impl.RoomServiceImpl;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.PageRequest;
@@ -42,25 +43,44 @@ public class ZzService {
      * 리턴 값 :
      * 기  능 :
      * */
-    public Long principalToHotelroomNum(Principal principal){
-        String email = principal.getName();
-        Long memberNum = memberRepository.findByMemberEmail(email).getMemberNum();
-        Pageable pageable = PageRequest.of(0,1, Sort.by(Sort.Direction.DESC,"roomNum"));
-        HotelRoomDTO hotelRoomDTO = roomService.getHotelRoomsByMember(memberNum,pageable).stream().findFirst().orElseThrow(EntityNotFoundException::new);
-        return hotelRoomDTO.getHotelRoomNum();
-    }
+//    public Long principalToHotelroomNum(Principal principal){
+//        String email = principal.getName();
+//        Long memberNum = memberRepository.findByMemberEmail(email).getMemberNum();
+//        Pageable pageable = PageRequest.of(0,1, Sort.by(Sort.Direction.DESC,"roomNum"));
+//        HotelRoomDTO hotelRoomDTO = roomService.getHotelRoomsByMember(memberNum,pageable).stream().findFirst().orElseThrow(EntityNotFoundException::new);
+//        return hotelRoomDTO.getHotelRoomNum();
+//    }
 
-    //roomNum으로 이것저것 다 하기
-
-
-    //hotelroomNum으로 Member찾기
-    public Member hotelroomNumToMember(Long hotelroomNum){
-        Room room = roomService.readRoomByHotelRoomNum(hotelroomNum);
-        return room.getMember();
-    }
+//    //hotelroomNum으로 Member찾기
+//    public Member hotelroomNumToMember(Long hotelroomNum){
+//        Room room = roomService.readRoomByHotelRoomNum(hotelroomNum);
+//        return room.getMember();
+//    }
 
     public Company hotelroomNumToCompany(Long hotelroomNum){
         return hotelRoomRepository.findById(hotelroomNum).orElseThrow(EntityNotFoundException::new)
                 .getCompany();
+    }
+
+    //    //roomNum으로 이것저것 다 하기
+    //0. room
+    public Room sessionToRoom(HttpSession session){
+        Long roomNum = (Long) session.getAttribute("roomNum");
+        return roomRepository.findById(roomNum).orElseThrow(EntityNotFoundException::new);
+    }
+
+    //1. hotelroomNum 찾기
+    public Long sessionToHotelroomNum(HttpSession session){
+        return sessionToRoom(session).getHotelRoom().getHotelRoomNum();
+    }
+
+    //2. Member 찾기
+    public Member sessionToMember(HttpSession session){
+        return sessionToRoom(session).getMember();
+    }
+
+    //3. Company 찾기
+    public Company sessionToCompany(HttpSession session){
+        return sessionToRoom(session).getHotelRoom().getCompany();
     }
 }
