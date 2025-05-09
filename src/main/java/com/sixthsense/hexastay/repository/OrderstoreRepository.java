1aa9f965bfb3d53c84e11a120b1e8bfa3ec45b56
예령 <gimdPfud@gmail.com>
2025-05-09 17:14:22 +0900

    .

--- src/main/java/com/sixthsense/hexastay/repository/OrderstoreRepository.java
+++ src/main/java/com/sixthsense/hexastay/repository/OrderstoreRepository.java
@@ -37,10 +37,10 @@ public interface OrderstoreRepository extends JpaRepository<Orderstore, Long> {
             Pageable pageable);
     
     /*페이지로 가져오기*/
-    Page<Orderstore> findByRoom_Member_MemberEmail (String email, Pageable pageable);
+//    Page<Orderstore> findByRoom_Member_MemberEmail (String email, Pageable pageable);
     Page<Orderstore> findByRoom_RoomNum (Long roomNum, Pageable pageable);
     /*리스트로 가져오기. 근데 주문내역이면........... 계속 나오지않나*/
-    List<Orderstore> findByRoom_Member_MemberEmail (String email);
+//    List<Orderstore> findByRoom_Member_MemberEmail (String email);
     List<Orderstore> findByRoom_RoomNum (Long roomNum);
 
     /*스토어넘버로 가져오기*/
