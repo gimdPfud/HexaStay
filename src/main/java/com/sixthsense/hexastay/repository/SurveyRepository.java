9e6bd8e7c381c723d7856cda0e7faa901d905a50
book <pik0329@naver.com>
2025-05-12 17:50:50 +0900

    커밈ㅅ무ㅏㅣ움니ㅏㅇ

--- src/main/java/com/sixthsense/hexastay/repository/SurveyRepository.java
+++ src/main/java/com/sixthsense/hexastay/repository/SurveyRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     // 활성화된 설문조사 조회
     List<Survey> findBySurveyIsActiveTrue();
+    
+    // 활성화된 설문조사 중 하나만 조회 (최신순)
+    Optional<Survey> findTopBySurveyIsActiveTrueOrderBySurveyNumDesc();
+    
+    // 회사별 설문조사 조회
+    List<Survey> findByCompany_CompanyNum(Long companyNum);
 }
