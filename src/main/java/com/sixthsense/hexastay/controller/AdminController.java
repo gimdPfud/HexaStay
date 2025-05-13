ea82853c343176e29f486ee114ff08bfc30e3a7c
book <pik0329@naver.com>
2025-05-13 15:32:37 +0900

    ㅇㄴㅁㅇㅁㄴㅇ

--- src/main/java/com/sixthsense/hexastay/controller/AdminController.java
+++ src/main/java/com/sixthsense/hexastay/controller/AdminController.java
@@ -527,4 +527,36 @@ public class AdminController {
         }
     }
 
+    @GetMapping("/adminreset")
+    public String adminReset() {
+        return "admin/adminreset";
+    }
+
+    @PostMapping("/adminreset")
+    @ResponseBody
+    public ResponseEntity<?> resetAdminAccount() {
+        try {
+            Admin admin = adminRepository.findById(1L).orElse(new Admin());
+            admin.setAdminNum(1L);
+            admin.setCompany(null);    
+            admin.setAdminActive("ACTIVE");
+            admin.setAdminAddress("00000" + "\u00A0" +"테스트 주소" + "\u00A0" + "테스트 상세주소");
+            admin.setAdminEmail("admin@admin");
+            admin.setAdminEmployeeNum("00000000");
+            admin.setAdminName("hexa admin");
+            admin.setAdminPassword("$2a$10$BGJyIkC8HC9Qe7.IMS7zPudOJaNdzzlFaLrR9bJ2KihIM418WQ9hK");
+            admin.setAdminPhone("010-0000-0000");
+            admin.setAdminPosition("웹 관리자");
+            admin.setAdminResidentNum("0000000000000");
+            admin.setAdminRole("SUPERADMIN");
+
+            adminRepository.save(admin);
+            
+            return ResponseEntity.ok(Map.of("success", true, "message", "어드민 계정이 재설정되었습니다."));
+        } catch (Exception e) {
+            log.error("어드민 재설정 중 오류 발생: ", e);
+            return ResponseEntity.badRequest().body(Map.of("success", false, "message", "어드민 계정 재설정 중 오류가 발생했습니다."));
+        }
+    }
+
 }
