046181752ebf421f2fec67370589d5034588060c
예령 <gimdPfud@gmail.com>
2025-05-14 19:38:11 +0900

    HexastayApplication.java 부활 SixthsenseApplication.java 안녕

--- src/main/java/com/sixthsense/hexastay/HexastayApplication.java
+++ src/main/java/com/sixthsense/hexastay/HexastayApplication.java
@@ -1,15 +1,15 @@
-//package com.sixthsense.hexastay;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//
-//@SpringBootApplication
-//@EnableJpaAuditing
-//public class HexastayApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(HexastayApplication.class, args);
-//    }
-//
-//}
+package com.sixthsense.hexastay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HexastayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexastayApplication.class, args);
+    }
+
+}
